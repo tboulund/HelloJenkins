e2dfcc79e4553739e312a40a1270e671fee94ae4
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2020-12-10 10:03:11 +0100

    Initial commit

---
+++ Test/DemoUnitTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class DemoUnitTests
+    {
+        [TestMethod]
+        public void BooleanAssertion()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
